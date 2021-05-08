16374530da013e36f597e3f95834d98a7941ab3f
Pavel <Pavel@sobaka.ru>
2021-05-08 20:50:53 +0300

    build: добавлены модульные тесты программы

--- lab-03.cpp
+++ lab-03.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include "Header.h"
 
 using namespace std;
 
@@ -12,20 +13,6 @@ vector<double> input_numbers(size_t count)
         cin >> result[i];
     }
     return result;
-}
-
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
 }
 
 vector <size_t> make_histogram(const vector<double>& numbers, size_t bin_count)
